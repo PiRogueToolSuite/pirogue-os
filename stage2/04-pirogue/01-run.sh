83133d6f676fcc3c6bdd1fdb799f2cf0f985fa82
U039b <git@0x39b.fr>
2022-06-12 15:22:21 +0200

    Switch to pts-project.org domain

--- stage2/04-pirogue/01-run.sh
+++ stage2/04-pirogue/01-run.sh
@@ -8,8 +8,8 @@ EOF
 ###
 # Install PiRogue packages
 on_chroot << EOF
-curl -o /etc/apt/sources.list.d/pirogue-os.list "https://piroguetoolsuite.github.io/ppa/pirogue.list"
-curl -o /etc/apt/trusted.gpg.d/pirogue-os.asc "https://piroguetoolsuite.github.io/ppa/Key.gpg"
+curl -o /etc/apt/sources.list.d/pirogue-os.list "https://pts-project.org/ppa/pirogue.list"
+curl -o /etc/apt/trusted.gpg.d/pirogue-os.asc "https://pts-project.org/ppa/Key.gpg"
 apt update
 
 DEBIAN_FRONTEND=noninteractive apt install -y pirogue-base
